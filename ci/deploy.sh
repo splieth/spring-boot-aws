b22a3a27c1c867541c829b53bd13bffa5dd70baf
Matthias Splieth <matthias@splieth.com>
2017-05-08 18:08:38 +0200

    Add deploy-script

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -2,4 +2,28 @@
 
 set -e
 
+SCRIPT_DIR=$(dirname ${0})
+
+if [ -z ${STACK_NAME} ]; then
+  echo "STACK_NAME is empty, exiting..."
+  exit 1
+fi
+
+if [ -z ${APPLICATION} ]; then
+  echo "APPLICATION is empty, exiting..."
+  exit 1
+fi
+
+if [ -z ${TEMPLATE_NAME} ]; then
+  echo "TEMPLATE_NAME is empty, exiting..."
+  exit 1
+fi
+
+REGION=${REGION:-'eu-central-1'}
+
 bundle check || bundle install
+
+bundle exec autostacker24 update --stack ${STACK_NAME} \
+                                 --region ${REGION} \
+                                 --template ${SCRIPT_DIR}/cf-templates/${TEMPLATE_NAME}.yaml \
+                                 --param Application=${APPLICATION}
