7fb6adf623f06f0f91856b1ea64b5d7f1ed8eb57
Matthias Splieth <matthias@splieth.com>
2017-05-14 23:01:46 +0200

    Add tests

--- src/test/java/com/book/app/repository/BookRepositoryListTest.java
+++ src/test/java/com/book/app/repository/BookRepositoryListTest.java
@@ -4,6 +4,8 @@ import com.book.app.domain.Book;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.util.Optional;
+
 import static org.testng.Assert.*;
 
 
@@ -11,8 +13,9 @@ public class BookRepositoryListTest {
 
     private BookRepositoryList bookRepo;
 
+    // insert
     @Test
-    public void shouldInsert() throws Exception {
+    public void insertShouldInsert() throws Exception {
         bookRepo = new BookRepositoryList();
         String someAuthor = "Some Author";
         String someTitle = "Some Title";
@@ -23,5 +26,49 @@ public class BookRepositoryListTest {
         assertEquals(someResponse.getAuthor(), someAuthor);
         assertEquals(someResponse.getTitle(), someTitle);
         assertEquals(someResponse.getPrice(), somePrice);
+    }
+
+    // findOne
+    @Test
+    public void findOnefindsObject() throws Exception {
+        bookRepo = new BookRepositoryList();
+        String someAuthor = "Some Author";
+        String someTitle = "Some Title";
+        Double somePrice = 199.99;
+        Book someBook = new Book(someAuthor, someTitle, somePrice);
+        Optional<Book> result = bookRepo.findOne(bookRepo.insert(someBook).getId());
+
+        assertTrue(result.isPresent());
+        assertEquals(result.get(), someBook);
+    }
+
+    @Test
+    public void findOnefindsNothing() throws Exception {
+        bookRepo = new BookRepositoryList();
+        Optional<Book> result = bookRepo.findOne("some-id");
+
+        assertFalse(result.isPresent());
+    }
+
+    // list
+    @Test
+    public void listIsEmpty() {
+        bookRepo = new BookRepositoryList();
+
+        assertTrue(bookRepo.list().isEmpty());
+    }
+
+    @Test
+    public void listContainsElements() {
+        bookRepo = new BookRepositoryList();
+        Book someBook = new Book("Some Author", "Some Title", 19.99);
+        Book someOtherBook = new Book("Some Other Author", "Some Strange Title", 99.99);
+        bookRepo.insert(someBook);
+        bookRepo.insert(someOtherBook);
+
+        assertFalse(bookRepo.list().isEmpty());
+        assertTrue(bookRepo.list().size() == 2);
+        assertEquals(bookRepo.list().get(0).getTitle(), "Some Title");
+        assertEquals(bookRepo.list().get(1).getTitle(), "Some Strange Title");
     }
 }
